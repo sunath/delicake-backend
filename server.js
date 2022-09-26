523c878d8da0f74c6cfe3dd2fa56a37fac9d9bc7
sunath <sunath2007@gmail.com>
2022-09-26 17:43:17 +0530

    First one

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const express = require("express");
 const mongoose = require("mongoose")
 const bodyParser = require("body-parser")
 
+const cors = require("cors")
+
+
+
 
 // Connect to the mongodb database
 const connectDB = async () => {
